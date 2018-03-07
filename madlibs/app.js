723aabe2e88501131f978640b516e8ce3cd434ba
Xiuling Zhang <xiuling.zhang@f1006-26-34158.bmcc.cuny.edu>
2018-03-07 17:40:16 -0500

    add 8ball, madlibs, quiz and password files

--- madlibs/app.js
+++ madlibs/app.js
@@ -1,23 +1,32 @@
-//html element references
-
 const submitButton = document.getElementById("submit");
 const exclamationInput = document.getElementById("exclamation");
 const adverbInput = document.getElementById("adverb");
 const nounInput = document.getElementById("noun");
 const adjectiveInput = document.getElementById("adjective");
 
-//listen for button press
+const message = document.createElement("p");
+	  document.body.appendChild(message);
 
-submitButton.onclick = function (){
-    const message = document.createElement("p");
-    message.textContent = exclamationInput.value
-                          +" he said "
-                          +adverbInput.value
-                          +" as he jumped into his convertible"
-                          +nounInput.value
-                          +" and drove off with his "
-                          +adjectiveInput.value
-                          +" wife. "
-    document.body.appendChild(message);
-
-}
+submitButton.onclick = function() {
+	
+	if (!exclamationInput.value) {
+		message.textContent = "You forgot to add an exclamation!";
+	} else if (!adverbInput.value) {
+        message.textContent = "You forgot to add an adverb!"
+    }else if (!nounInput.value) {
+        message.textContent = "You forgot to add an noun!"
+    }else if (!adjectiveInput.value) {
+        message.textContent = "You forgot to add an adjective!"
+    }
+    else {
+        message.textContent = exclamationInput.value
+							+ " he said "
+							+ adverbInput.value
+							+ " as he jumped into his convertible "
+							+ nounInput.value
+							+ " and drove off with his "
+							+ adjectiveInput.value
+							+ " wife."
+	}
+	
+};
